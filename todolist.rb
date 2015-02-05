10cc6faa9cddb7fde90e67f513495092b426180d
ljnissen <lj.nissen@gmail.com>
2015-02-05 19:26:18 +0000

    Server still does not work.

--- todolist.rb
+++ todolist.rb
@@ -1,21 +1,21 @@
 require 'sinatra'
 require 'data_mapper'
 
-get '/' do
-	@notes = Note.all :order => :id.desc
-	@title = 'All notes'
-	erb :home
-end
-
-DataMapper::setup(:default, "sqlite3://#{Dir.pwd}/todolist.rb")
+DataMapper::setup(:default, "sqlite3://#{Dir.pwd}/todolist.db")
 
 class Note
 	include DataMapper::Resource
 	property :id, Serial
 	property :content, Text, :required => true
-	property :complete, Boolean, :required => true, default => false
+	property :complete, Boolean, :required => true, :default => false
 	property :created_at, DateTime
 	property :updated_at, DateTime
 end
 
 DataMapper.finalize.auto_upgrade!
+
+get '/' do
+	@notes = Note.all :order => :id.desc
+	@title = 'All notes'
+	erb :home
+end
